6c8278d8a0c7050c2eda1d4f4f1da3966b3dcd72
jay lu <jayjjlu@gmail.com>
2017-08-27 21:14:31 +0800

    template done

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+
+gulp.task('connect', function(){
+  connect.server({
+    root: 'public',
+    livereload: true
+  });
+});
+
+// keeps gulp from crashing for scss errors
+gulp.task('sass', function () {
+  return gulp.src('./sass/*.scss')
+      .pipe(sass({ errLogToConsole: true }))
+      .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('livereload', function (){
+  gulp.src('./public/**/*')
+  .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./public/**/*', ['livereload']);
+});
+
+gulp.task('open',['connect', 'watch', 'sass'], function(){
+  gulp.src('')
+  .pipe(open({uri: 'http://localhost:8080/index.html', app: 'Google Chrome'}));
+});
+
+gulp.task('default', ['open']);
+
